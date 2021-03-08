1b867eebc5ab64200826fdc1dffd3667ac7f2519
RaviHelon-msf <57137468+RaviHelon-msf@users.noreply.github.com>
2021-03-08 12:27:17 -0300

    Definição de Parâmetros e criação do laço principal

--- Modelo.jl
+++ Modelo.jl
@@ -1,1 +1,55 @@
-println("fuck you all")
+include("""Parametros.jl""")
+
+t=0
+
+# Valores iniciais
+p_la = [0] # Pressão átrio esquerdo
+V_lv = [0] # Volume ventrículo esquerdo
+p_as = [0] # Pressão arterial sistêmica
+p_vs = [0] # Pressão venosa sistêmica
+p_ra = [0] # Pressão átrio direito
+V_rv = [0] # Volume ventrícular direito
+p_ap = [0] # Pressão arterial pulmonar
+p_vp = [0] # Pressão venosa pulmonar
+q_as = [0] # Fluxo arterial sistêmico
+q_vs = [0] # Fluxo venoso sistêmico
+q_ap = [0] # Fluxo arterial pulmonar
+q_vp = [0] # Fluxo venoso pulmonar
+
+x = [p_la, V_lv, p_as, p_vs, p_ra, V_rv, p_ap, p_vp, q_as, q_vs, q_ap, q_vp]
+
+
+# Laço principal
+while t<T
+    # Valores incrementais
+    dp_la =
+    dV_lv =
+    dp_as =
+    dp_vs =
+    dp_ra =
+    dV_rv =
+    dp_ap =
+    dp_vp =
+    dq_as =
+    dq_vs =
+    dq_ap =
+    dq_vp =
+
+    # Dinâmica
+    push!(p_la,p_la[end]+dp_la)
+    push!(V_lv,V_lv[end]+dV_lv)
+    push!(p_as,p_as[end]+dp_as)
+    push!(p_vs,p_vs[end]+dp_vs)
+    push!(p_ra,p_ra[end]+dp_ra)
+    push!(V_rv,V_rv[end]+dV_rv)
+    push!(p_ap,p_ap[end]+dp_ap)
+    push!(p_vp,p_vp[end]+dp_vp)
+    push!(q_as,q_as[end]+dq_as)
+    push!(q_vs,q_vs[end]+dq_vs)
+    push!(q_ap,q_ap[end]+dq_ap) 
+    push!(q_vp,q_vp[end]+dq_vp)
+
+    t = t+h
+end
+
+# Interface Final
---
+++ Parametros.jl
@@ -0,0 +1,45 @@
+# Parâmetros de Simulação
+T = 8 # Tempo em segundos
+h = 1e-5 # Passo de cálculo em segundos
+
+
+
+
+
+# Lado Direito do Coração
+C_ra = [5, 5] # ml/mmHg
+D_tv = [1, 1] # Diodo
+R_tv = [5e-3, 5e-3] # mmHg.s/ml
+E_s_rv = [1.15, 0.5] # mmHg/ml
+E_d_rv = [0.1, 0.1] # mmHg/ml
+D_pv = [1, 1] # Diodo
+R_pv = [3e-3, 3e-3] # mmHg.s/ml
+
+# Lado Esquerdo do Coração
+C_la = [5, 5] # ml/mmHg
+D_mv = [1, 1] # Diodo
+R_mv = [5e-3, 5e-3] # mmHg.s/ml
+E_s_lv = [2.5, 0.5] # mmHg/ml
+E_d_lv = [0.1, 0.1] # mmHg/ml
+D_av = [1, 1] # Diodo
+R_av = [8e-3, 8e-3] # mmHg.s/ml
+
+
+# Circulação Sistêmica
+C_as = [1.5, 1.5] # ml/mmHg
+R_as = [1, 1.4] # mmHg.s/ml
+L_as = [1e-5, 1e-5] # mmHg.s2/ml
+C_vs = [82.5, 82.5] # ml/mmHg
+R_vs = [0.09, 0.09]# mmHg.s/ml
+L_vs = [1e-5, 1e-5] # mmHg.s2/ml
+
+# Circualação Pulmonar
+C_ap = [3, 3] # ml/mmHg
+R_ap = [3e-3, 3e-3] # mmHg.s/ml
+L_ap = [1e-5, 1e-5] # mmHg.s2/ml
+C_vp = [5, 5] # ml/mmHg
+R_vp = [0.07, 0.07] # mmHg.s/ml
+L_vp = [1e-5, 1e-5] # mmHg.s2/ml
+
+
+# Dispositivo de Assistência Ventricular
