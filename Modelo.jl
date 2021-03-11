7292236c3b618ec1e0a951ec9007270db8f6bc26
RaviHelon-msf <57137468+RaviHelon-msf@users.noreply.github.com>
2021-03-11 10:56:28 -0300

    Funcional 1.0

--- Modelo.jl
+++ Modelo.jl
@@ -15,14 +15,27 @@ q_as = [0] # Fluxo arterial sistêmico
 q_vs = [0] # Fluxo venoso sistêmico
 q_ap = [0] # Fluxo arterial pulmonar
 q_vp = [0] # Fluxo venoso pulmonar
+p_lv = [0]
+p_rv = [0]
 
 # Laço principal
 for t in h:h:T
+    t_m = t%T_c
 
-    p_lv =
-    p_as =
-    p_rv =
-    p_ap = 
+    if t_m<T_s:
+        E_m = 1 - cos(pi*t_m/T_s)
+    elseif (t_m<T_d)&&(t_m>T_s):
+        E_m = 1 + cos(pi*(t_m - T_s)/(T_d-T_s))
+    else
+        E_m = 0
+    end
+
+    E_rv = E_d_rv - 0.5*(E_s_rv - E_d_rv)*E_m
+    E_lv = E_d_lv - 0.5*(E_s_lv - E_d_lv)*E_m
+
+    push!(p_lv,P_lv[end-1] + E_lv*(V_lv[end] - V_lv[end-1]))
+    push!(p_rv,P_rv[end-1] + E_lv*(V_rv[end] - V_rv[end-1]))
+
 
     # Modelagem dos Diodos
     D_mv = (p_la[end]-p_lv>0) ? true: false
@@ -30,34 +43,41 @@ for t in h:h:T
     D_tv = (p_ra[end]-p_rv>0) ? true: false
     D_pv = (p_rv-p_ap>0) ? true: false
 
+    #Fluxos Importantes
+    q_mv = (D_mv/R_mv)*(p_la[end]-p_lv)
+    q_av = (D_av/R_av)*(p_lv-p_as[end])
+    q_tv = (D_mv/R_mv)*(p_ra[end]-p_rv)
+    q_pv = (D_mv/R_mv)*(p_rv-p_ap[end])
 
     # Valores incrementais
-    dp_la =
-    dV_lv =
-    dp_as =
-    dp_vs =
-    dp_ra =
-    dV_rv =
-    dp_ap =
-    dp_vp =
-    dq_as =
-    dq_vs =
-    dq_ap =
-    dq_vp =
+    dp_la = (1/C_la)*(q_vp[end] - q_mv)
+    dV_lv = (q_mv - q_av)
+    dp_as = (1/C_as)*(q_av - q_as[end])
+    dp_vs = (1/C_vs)*(q_as[end] - q_vs[end])
+    dp_ra = (1/C_ra)*(q_vs[end] - q_tv)
+    dV_rv = (q_tv - q_pv)
+    dp_ap = (1/C_ap)*(q_pv - q_ap[end])
+    dp_vp = (1/C_vp)*(q_ap[end] - q_vp[end])
+    dq_as = (1/L_as)*(q_as[end]+(p_vs[end]-p_as[end])/R_as)
+    dq_vs = (1/L_vs)*(q_vs[end]+(p_ra[end]-p_vs[end])/R_vs)
+    dq_ap = (1/L_ap)*(q_ap[end]+(p_vp[end]-p_ap[end])/R_ap)
+    dq_vp = (1/L_vp)*(q_vp[end]+(p_la[end]-p_vp[end])/R_vp)
 
     # Dinâmica
-    push!(p_la,p_la[end]+dp_la)
-    push!(V_lv,V_lv[end]+dV_lv)
-    push!(p_as,p_as[end]+dp_as)
-    push!(p_vs,p_vs[end]+dp_vs)
-    push!(p_ra,p_ra[end]+dp_ra)
-    push!(V_rv,V_rv[end]+dV_rv)
-    push!(p_ap,p_ap[end]+dp_ap)
-    push!(p_vp,p_vp[end]+dp_vp)
-    push!(q_as,q_as[end]+dq_as)
-    push!(q_vs,q_vs[end]+dq_vs)
-    push!(q_ap,q_ap[end]+dq_ap)
-    push!(q_vp,q_vp[end]+dq_vp)
+    push!(p_la,p_la[end]+dp_la*h)
+    push!(V_lv,V_lv[end]+dV_lv*h)
+    push!(p_as,p_as[end]+dp_as*h)
+    push!(p_vs,p_vs[end]+dp_vs*h)
+    push!(p_ra,p_ra[end]+dp_ra*h)
+    push!(V_rv,V_rv[end]+dV_rv*h)
+    push!(p_ap,p_ap[end]+dp_ap*h)
+    push!(p_vp,p_vp[end]+dp_vp*h)
+    push!(q_as,q_as[end]+dq_as*h)
+    push!(q_vs,q_vs[end]+dq_vs*h)
+    push!(q_ap,q_ap[end]+dq_ap*h)
+    push!(q_vp,q_vp[end]+dq_vp*h)
 end
 
 # Interface Final
+gr()
+plot(p_lv)
--- Parametros.jl
+++ Parametros.jl
@@ -2,40 +2,54 @@
 T = 8 # Tempo em segundos
 h = 1e-5 # Passo de cálculo em segundos
 
+# Parâmetros Geerais
+p_lv_es = 124.56
+p_lv_ed = 13.43
+p_as_es = 123.59
+p_as_ed = 83.35
+V_lv_es = 49.75
+V_lv_ed = 126.4
+HR = 66
+T_c = 0.92
+T_s = 0.28
+T_d = 0.41
+SV = 77
+CO = 5.08
+
 # Lado Direito do Coração
-C_ra = [5, 5] # ml/mmHg
-D_tv = [1, 1] # Diodo
-R_tv = [5e-3, 5e-3] # mmHg.s/ml
-E_s_rv = [1.15, 0.5] # mmHg/ml
-E_d_rv = [0.1, 0.1] # mmHg/ml
-D_pv = [1, 1] # Diodo
-R_pv = [3e-3, 3e-3] # mmHg.s/ml
+C_ra = 5 # ml/mmHg
+D_tv = 1 # Diodo
+R_tv = 5e-3 # mmHg.s/ml
+E_s_rv = 1.15 # mmHg/ml
+E_d_rv = 0.1 # mmHg/ml
+D_pv = 1 # Diodo
+R_pv =  3e-3 # mmHg.s/ml
 
 # Lado Esquerdo do Coração
-C_la = [5, 5] # ml/mmHg
-D_mv = [1, 1] # Diodo
-R_mv = [5e-3, 5e-3] # mmHg.s/ml
-E_s_lv = [2.5, 0.5] # mmHg/ml
-E_d_lv = [0.1, 0.1] # mmHg/ml
-D_av = [1, 1] # Diodo
-R_av = [8e-3, 8e-3] # mmHg.s/ml
+C_la = 5 # ml/mmHg
+D_mv = 1 # Diodo
+R_mv = 5e-3 # mmHg.s/ml
+E_s_lv = 2.5 # mmHg/ml
+E_d_lv = 0.1 # mmHg/ml
+D_av = 1 # Diodo
+R_av = 8e-3 # mmHg.s/ml
 
 
 # Circulação Sistêmica
-C_as = [1.5, 1.5] # ml/mmHg
-R_as = [1, 1.4] # mmHg.s/ml
-L_as = [1e-5, 1e-5] # mmHg.s2/ml
-C_vs = [82.5, 82.5] # ml/mmHg
-R_vs = [0.09, 0.09]# mmHg.s/ml
-L_vs = [1e-5, 1e-5] # mmHg.s2/ml
+C_as = 1.5 # ml/mmHg
+R_as = 1 # mmHg.s/ml
+L_as = 1e-5 # mmHg.s2/ml
+C_vs = 82.5 # ml/mmHg
+R_vs = 0.09# mmHg.s/ml
+L_vs = 1e-5 # mmHg.s2/ml
 
 # Circualação Pulmonar
-C_ap = [3, 3] # ml/mmHg
-R_ap = [3e-3, 3e-3] # mmHg.s/ml
-L_ap = [1e-5, 1e-5] # mmHg.s2/ml
-C_vp = [5, 5] # ml/mmHg
-R_vp = [0.07, 0.07] # mmHg.s/ml
-L_vp = [1e-5, 1e-5] # mmHg.s2/ml
+C_ap = 3# ml/mmHg
+R_ap = 3e-3 # mmHg.s/ml
+L_ap = 1e-5 # mmHg.s2/ml
+C_vp = 5 # ml/mmHg
+R_vp = 0.07 # mmHg.s/ml
+L_vp = 1e-5 # mmHg.s2/ml
 
 
 # Dispositivo de Assistência Ventricular
