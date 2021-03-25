e353eb3f1b07146aff431832e9dbdc230f7558c5
RaviHelon-msf <57137468+RaviHelon-msf@users.noreply.github.com>
2021-03-25 12:38:49 -0300

    Alpha 0.5.2

--- main.jl
+++ main.jl
@@ -4,6 +4,7 @@ using DifferentialEquations
 
 include("""Parametros_Sistemico.jl""")
 include("""inputs.jl""")
+include("""Modelo_Sistemico.jl""")
 
 teste = """default"""
 alg = RK4()
@@ -28,30 +29,64 @@ sol = solve(problem, alg)
 
 gr()
 
-plot_press = plot(sol, label = """Câmara de ar""", vars = (0,1))
-plot_press = plot!(sol, label = """Complacência câmera de sangue""", vars = (0,2))
-plot_press = plot!(sol, label = """Aorta""", vars = (0,3))
-title!("""Pressões versus tempo""")
-ylabel!("""Pressões (mmHg)""")
+plot_press = plot(sol, vars = (0,1))
+title!("""Pressão na Câmara de ar""")
+ylabel!("""Pressão (mmHg)""")
 xlabel!("""Tempo (s)""")
 
 
-#filename1 = """Pressões""" + teste + string(today())
-#png(plot_press, filename1)
+filename1 = """Pca"""*teste*string(today())
+png(plot_press, filename1)
+
+
+plot_press = plot(sol, vars = (0,2))
+title!("""Pressão na Complacência câmera de sangue""")
+ylabel!("""Pressão (mmHg)""")
+xlabel!("""Tempo (s)""")
+
+
+filename1 = """Pccs"""*teste*string(today())
+png(plot_press, filename1)
+
+
+plot_press = plot(sol, vars = (0,3))
+title!("""Pressão na Aorta""")
+ylabel!("""Pressão (mmHg)""")
+xlabel!("""Tempo (s)""")
+
+
+filename1 = """Pao"""*teste*string(today())
+png(plot_press, filename1)
 
 # x_0[1] = [Pca] # Pressão camara de ar
 # x_0[2] = [Pcs] # Pressão Complacência camera de sangue
 # x_0[3] = [Pao] # Pressão Aortica
 
-plot_flux =  plot(sol, label = """Cânula de entrada""",vars = (0,4))
-plot_flux =  plot!(sol, label = """Cânula de saída""",vars = (0,5))
-plot_flux =  plot!(sol, label = """Sistêmico""",vars = (0,6))
-title!("""Fluxos versus tempo""")
-ylabel!("""Fluxos (ml/s)""")
+plot_flux =  plot(sol, vars = (0,4))
+title!("""Fluxo na Cânula de entrada""")
+ylabel!("""Fluxo (ml/s)""")
 xlabel!("""Tempo (s)""")
 
-#filename1 = """Fluxos""" + teste + string(today())
-#png(plot_press, filename1)
+filename1 = """qi"""*teste*string(today())
+png(plot_flux, filename1)
+
+
+plot_flux =  plot(sol,vars = (0,5))
+title!("""Fluxo na Cânula de saída""")
+ylabel!("""Fluxo (ml/s)""")
+xlabel!("""Tempo (s)""")
+
+filename1 = """qo"""*teste*string(today())
+png(plot_flux, filename1)
+
+
+plot_flux =  plot(sol, vars = (0,6))
+title!("""Fluxo Sistêmico""")
+ylabel!("""Fluxo (ml/s)""")
+xlabel!("""Tempo (s)""")
+
+filename1 = """qs """*teste*string(today())
+png(plot_flux, filename1)
 
 # x_0[4] = [Qin] # Fluxo arterial sistêmico
 # x_0[5] = [Qout] # Fluxo venoso sistêmico
